273e0f7ef5acb4d91b198b46c709b631de0b9bc7
Ben Payne <bpayne@jetheaddev.com>
2015-04-30 20:03:19 -0700

    Several fixes to the mixing algorithm, it is now functioning as expected.
    Also added some notes for items that still need to be taken care of.

--- arduino/rduomoto/rduomoto.ino
+++ arduino/rduomoto/rduomoto.ino
@@ -61,9 +61,13 @@ void loop()
 {
   int vert_val;
   int hor_val;
-  int left_val;
-  int right_val;
+  long left_val;
+  long right_val;
+  int mm_val;
   
+  // Get the pulses for the channels.
+  // TODO: Add 3rd channel for positive engagement 
+  // TODO: Add 4th channel to allow web control
   ch1 = pulseIn(ch1_pin,HIGH);
   ch2 = pulseIn(ch2_pin,HIGH);
   
@@ -72,16 +76,19 @@ void loop()
   Serial.print( "Ch2: " );
   Serial.println( ch2 );
   
-  if ( ch1 < 1000 && ch2 < 1000 )
+  // Check if values are within range, otherwise full stop.
+  if ( ch1 < 1000 || ch2 < 1000 || ch1 > 2000 || ch2 > 2000 )
   {
     vert_val = 0;
     hor_val = 0;
   }
   else 
   {
+    // Turn channel timing data into a value between -255 and +255 for both channels
+    //  Vert is forward and back.
     if ( ch1 >= 1500 )
     {
-      vert_val=map( ch1, 1500, 1900, 0, 255 );
+      vert_val=-map( ch1, 1500, 1900, 0, 255 );
       Serial.print( "Out forward ch1: " );
       Serial.println( val );
       if ( vert_val > 255 )
@@ -89,16 +96,17 @@ void loop()
     }
     else
     {
-      vert_val=-map( ch1, 1500, 1100, 0, 255 );
+      vert_val=map( ch1, 1500, 1100, 0, 255 );
       Serial.print( "Out backward ch1: " );
       Serial.println( vert_val );
       if ( vert_val < -255 )
         vert_val = -255;
     }
 
+    // Hor is left/right
     if ( ch2 >= 1500 )
     {
-      hor_val=map( ch2, 1500, 2000, 0, 255 );
+      hor_val=-map( ch2, 1500, 2000, 0, 255 );
       Serial.print( "Out forward ch2: " );
       Serial.println( hor_val );
       if ( hor_val > 255 )
@@ -106,7 +114,7 @@ void loop()
     }
     else
     {
-      hor_val=-map( ch2, 1500, 1000, 0, 255 );
+      hor_val=map( ch2, 1500, 1000, 0, 255 );
       Serial.print( "Out backward ch2: " );
       Serial.println( hor_val );
       if ( hor_val < -255 )
@@ -114,28 +122,59 @@ void loop()
     }
   }
   
+  // Now mix the values together to get the speeds for the left and right motor.
+  if ( vert_val > 0 )
+  {
     if ( hor_val < 0 )
     {
-      left_val = vert_val - hor_val;
-      right_val = vert_val;
+      right_val = vert_val - hor_val;
+      left_val = vert_val;
     }
     else
     {
-      left_val = vert_val;
-      right_val = vert_val + hor_val;
+      right_val = vert_val;
+      left_val = vert_val + hor_val;
     }
+  }
+  else
+  {
+    if ( hor_val < 0 )
+    {
+      right_val = vert_val + hor_val;
+      left_val = vert_val;
+    }
+    else
+    {
+      right_val = vert_val;
+      left_val = vert_val - hor_val;
+    }
+  }
   
+  Serial.print( "Left: " );
+  Serial.print( left_val );
+  Serial.print( " Right: " );
+  Serial.println( right_val );
+  
+  // Now since the values may be larger that 255, we'll scale them back to within range.
   if ( max( right_val, left_val ) > 255 )
   {  
-    left_val = left_val * 255 / max( right_val, left_val );
-    right_val = right_val * 255 / max( right_val, left_val );
+    mm_val = max( right_val, left_val );
+    left_val = left_val * 255 / mm_val;
+    right_val = right_val * 255 / mm_val;
   }
   else if ( min( right_val, left_val ) < -255 )
   {  
-    left_val = left_val * -255 / max( right_val, left_val );
-    right_val = right_val * -255 / max( right_val, left_val );
+    mm_val =  min( right_val, left_val );
+    left_val = left_val * -255 / mm_val;
+    right_val = right_val * -255 / mm_val;
   }
   
+  Serial.print( "Scaled Left: " );
+  Serial.print( left_val );
+  Serial.print( " Scaled Right: " );
+  Serial.println( right_val );
+
+  // Now tell the motors what to do.
   if ( left_val < 0 )
     setLeftMotor( 0, -left_val );
   else
@@ -146,11 +185,13 @@ void loop()
   else
     setRightMotor( 1, right_val );
   
+  // TODO: Detect dead stike position and full stop.
   //delay(1000);
 }
 
 void stopped()
 { 
+  // TODO add control for the motor brakes.  
   digitalWrite(dir_a, LOW); //Set motor direction, 1 low, 2 high
   digitalWrite(dir_b, LOW); //Set motor direction, 3 high, 4 low
   analogWrite(pwm_a, 0);    //set both motors to run at 100% duty cycle (fast)
