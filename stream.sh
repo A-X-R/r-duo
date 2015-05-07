727bb234bcf31813e26b499382a4ccbcc22d8187
Jacob Oursland <jacob.oursland@gmail.com>
2015-05-06 21:16:05 -0700

    Source the Twitch.TV key from an external file.

--- stream.sh
+++ stream.sh
@@ -4,7 +4,10 @@ WIDTH=320
 HEIGHT=240
 FPS=30
 
-TWITCH_KEY="live_89916911_6FSHP4XbA7b7lE4ne1SPAkxZNuVKNr"
+# The file 'twitch.key' shall have the contents 'TWITCH_KEY="[key]"'
+# with the approrpriate key substituted
+source twitch.key
+
 LOCATION="rtmp://live.twitch.tv/app/$TWITCH_KEY live=1 flashver=FME/3.0%20(compatible;%20FMSc%201.0)"
 
 gst-launch-1.0 \
